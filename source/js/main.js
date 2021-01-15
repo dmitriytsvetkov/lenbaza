a84954c90956791aa0f13e631efecbbf31ce8bfe
dtsvetkovspb <44998241+dtsvetkovspb@users.noreply.github.com>
2021-01-15 11:02:29 +0300

    Закончил блок каталога на всех разрешениях

--- source/js/main.js
+++ source/js/main.js
@@ -148,22 +148,24 @@ const multipleSlider = new Swiper('.multiple-slider__container', {
 })
 
 const mobileSliderInit = () => {
-  if (window.innerWidth < 768 && promoSlider.dataset.mobile === 'false') {
-    mySwiper = new Swiper(promoSlider, {
-      slidesPerView: 1.2,
-      spaceBetween: 16,
-      slideClass: 'promo__slider-item',
-      wrapperClass: 'promo__slider-wrapper',
-    });
+  if (promoSlider) {
+    if (window.innerWidth < 768 && promoSlider.dataset.mobile === 'false') {
+      mySwiper = new Swiper(promoSlider, {
+        slidesPerView: 1.2,
+        spaceBetween: 16,
+        slideClass: 'promo__slider-item',
+        wrapperClass: 'promo__slider-wrapper',
+      });
 
-    promoSlider.dataset.mobile = 'true';
-  }
+      promoSlider.dataset.mobile = 'true';
+    }
 
-  if (window.innerWidth >= 768) {
-    promoSlider.dataset.mobile = 'false';
+    if (window.innerWidth >= 768) {
+      promoSlider.dataset.mobile = 'false';
 
-    if (promoSlider.classList.contains('swiper-container-initialized')) {
-      mySwiper.destroy();
+      if (promoSlider.classList.contains('swiper-container-initialized')) {
+        mySwiper.destroy();
+      }
     }
   }
 }
